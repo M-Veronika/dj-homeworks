c9c8c124820130d1c8ddbeae8c9ea1d2d42591eb
Morozova Veronika <veronika1520172@yandex.ru>
2025-02-20 20:12:32 +0300

    recipes

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,6 +19,37 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+def index_view(request):
+    # Передаем список рецептов в шаблон
+    context = {
+        'recipes': DATA
+    }
+    return render(request, 'calculator/index.html', context)
+
+def recipe_view(request, dish_name):
+    # Получаем количество порций из параметров запроса
+    servings = request.GET.get('servings', 1)  # По умолчанию 1 порция
+    try:
+        servings = int(servings)
+        if servings < 1:
+            raise ValueError
+    except ValueError:
+        return render(request, 'calculator/error.html', {'error': 'Параметр servings должен быть положительным целым числом.'})
+
+    # Получаем рецепт из DATA
+    recipe = DATA.get(dish_name)
+    if recipe is None:
+        return render(request, 'calculator/error.html', {'error': 'Рецепт не найден.'})
+
+    # Умножаем количество ингредиентов на количество порций
+    adjusted_recipe = {ingredient: quantity * servings for ingredient, quantity in recipe.items()}
+
+    # Передаем рецепт и название блюда в шаблон
+    context = {
+        'dish_name': dish_name,
+        'recipe': adjusted_recipe
+    }
+    return render(request, 'calculator/index.html', context)
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import index_view, recipe_view 
+
 urlpatterns = [
     # здесь зарегистрируйте вашу view-функцию
+    path('', index_view, name='index_view'),
+    path('<str:dish_name>/', recipe_view, name='recipe_view'),
 ]
